f6ffc3e2d4395d6c94b5a9571b120736c33a329c
vitinho <123788821+vitinhoqzy@users.noreply.github.com>
2024-05-17 20:04:41 -0300

    Update script.js

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -89,7 +89,7 @@ const handleLogin = (event) => {
     login.style.display = "none"
     chat.style.display = "flex"
 
-    websocket = new WebSocket("ws://localhost:8080")
+    websocket = new WebSocket("wss://chat-backend-9jla.onrender.com")
     websocket.onmessage = processMessage
 }
 
